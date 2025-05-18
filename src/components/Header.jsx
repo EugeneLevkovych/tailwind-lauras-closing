158080ae20f7057bfd2b4cfb05af9624a827e9f0
Eugene <eugenelevkovych@yahoo.com>
2025-05-18 15:17:40 +0300

    add glass

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
 const HEADER_LIST = ["Store", "Account", "Wish List", "Basket"];
+import { CiShoppingBasket } from "react-icons/ci";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function Header() {
   return (
     <header className="flex">
       <a href="#">
-        <h1 className=" text-xl" >Laura’s Closet</h1>
+        <h1 className="text-xl" >Laura’s Closet</h1>
       </a>
-        <input type="text" placeholder="Search for an item..." className="w-70 p-2 border" />
+      <div className="relative ml-auto mr-auto">
+        <div className="absolute bottom-2 left-0">
+      <FaMagnifyingGlass className="text-gray-400"/>
+        </div>
+        <input type="text" placeholder="Search for an item..." className="w-70 pl-8  border-b-2 border-gray-500 focus:border-blue-500 outline-none transition-colors" />
+        </div>
         <ul className="flex space-x-4">
-          {HEADER_LIST.map((item)=><li className="" key={item}>{item}</li>)}
+          {HEADER_LIST.map((item)=><li className="hover:text-blue-500"   key={item}>{item}</li>)}
         </ul>
+        <CiShoppingBasket className="h-6 w-6"/>
     </header>
   )
 }
