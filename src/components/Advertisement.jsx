d513204762499c835251950c25bc00bfad70309b
Eugene <eugenelevkovych@yahoo.com>
2025-05-20 14:31:33 +0300

    add number of prod

--- src/components/Advertisement.jsx
+++ src/components/Advertisement.jsx
@@ -1,14 +1,14 @@
 export default function Advertisement() {
     return (
     <div className="flex max-w-(--my-break-xl) mx-auto px-4">
-      <div className="flex text-white w-full bg-stone-800">
+      <div className="flex text-white w-full bg-stone-800 overflow-hidden">
           <div className="p-5 mr-auto">
             <h3 className="font-bold">Ladies Top</h3>
             <p className="font-light">Slash Sales begins in April.Get up to 80% Discount on all products.<span className="font-bold">Read More</span></p>
           </div>
             
-          <div className="bg-blue-300 w-14 -skew-x-30 -translate-x-6.5"></div>
-          <div className="bg-red-300 w-14 -skew-x-30 -translate-x-6.5"></div>
+          <div className="bg-blue-300 w-14 -skew-x-30 -translate-x-1"></div>
+          <div className="bg-red-300 w-14 -skew-x-30 -translate-x-1"></div>
       </div>
     </div> 
     )
--- src/components/NumberOfProducts.jsx
+++ src/components/NumberOfProducts.jsx
@@ -1,8 +1,8 @@
 export default function NumberOfProducts() {
     return (
-        <div className="flex justify-between max-w-(--my-break-xl) mx-auto px-4">
-            <div className="text-gray-dark">Showing 1-20 <span>out of 2,356 Products</span></div>
-            <div>Sort by</div>
+        <div className="flex justify-between max-w-(--my-break-xl) mx-auto px-4 py-12">
+            <div className="text-gray-dark font-semibold">Showing 1-20 <span className="text-gray-light font-normal">out of 2,356 Products</span></div>
+            <div>Sort by:</div>
         </div>
     )
 }
