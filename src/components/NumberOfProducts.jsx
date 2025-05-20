96c6e95d93ee89c895ae0d0885b720529bc07c99
Eugene <eugenelevkovych@yahoo.com>
2025-05-20 14:53:56 +0300

    add select

--- src/components/NumberOfProducts.jsx
+++ src/components/NumberOfProducts.jsx
@@ -2,7 +2,14 @@ export default function NumberOfProducts() {
     return (
         <div className="flex justify-between max-w-(--my-break-xl) mx-auto px-4 py-12">
             <div className="text-gray-dark font-semibold">Showing 1-20 <span className="text-gray-light font-normal">out of 2,356 Products</span></div>
-            <div>Sort by:</div>
+            <div className="space-x-1">
+                <label className="text-gray-light font-normal" for="sort-by">Sort by:</label>
+                <select className="text-gray-dark font-semibold" name="sort-by">
+                  <option value="New Arrivals">New Arrivals</option>
+                  <option value="Last Summer">Last Summer</option>
+                  <option value="Discont">Discont</option>
+                 </select>
+            </div>
         </div>
     )
 }
