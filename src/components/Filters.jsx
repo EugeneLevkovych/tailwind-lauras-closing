1509a52c5d518bfd19d86dd3632501c075944df5
Eugene <eugenelevkovych@yahoo.com>
2025-05-21 16:48:51 +0300

    add gap

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,15 +2,24 @@ export default function Filters() {
     return (
         <div>
     <p className="font-semibold">Filters</p>
-    <form className="flex-col gap-y-2.5 text-sm">
+    <form className="flex flex-col gap-y-3.5 text-sm">
+        <div className="flex gap-2">
        <input type="checkbox" name="women" value="women" />
        <label>Women</label>
-       <input type="checkbox" name="hobby" value="sports" />
-       <label>Sports</label>
-       <input type="checkbox" name="hobby" value="reading" />
-       <label>Reading</label>
+       </div>
+       <div className="flex gap-2">
+       <input type="checkbox" name="ladies" value="ladies" />
+       <label>Ladies</label>
+       </div>
+       <div className="flex gap-2">
+       <input type="checkbox" name="girls" value="girls" />
+       <label>Girls</label>
+       </div>
+        <div className="flex gap-2">
+       <input type="checkbox" name="babies" value="babies" />
+       <label>Babies</label>
+       </div>
     </form>
-
-        </div>
+    </div>
     )
 }
