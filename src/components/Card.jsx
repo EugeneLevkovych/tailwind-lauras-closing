a9410ab231b22ea595fcfb617d2a5a523d263810
Eugene <eugenelevkovych@yahoo.com>
2025-05-22 15:50:00 +0300

    add justify

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -69,10 +69,21 @@ export const PRODUCTS_DATA = [
 export default function Card({ productObj }) {
   
     return (
-        <li className="h-80 w-[calc((100%-40px)/3)] border border-solid">
-            <img className="h-full w-full" src={productObj.photoName} alt="Girl" />
-            <p>{productObj.name}</p>
-            <p>{productObj.price}</p>
-        </li>
+    <li className="h-[30.7rem] w-[calc((100%-40px)/3)] border border-solid">
+        <img className="h-2/3 w-full" src={productObj.photoName} alt="Girl" />
+         <div className="p-4 flex flex-col justify-between h-1/3">
+          <p className="text-gray-dark">{productObj.name}</p>
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+               <p className="text-lg font-semibold text-price-black">{productObj.price}</p>
+               <p className="text-lg font-semibold line-through text-gray-lighter">{productObj.price}</p>
+            </div>
+            <div>
+               <p className="text-gray-darker">24 otders</p>
+            </div>
+          </div>
+         </div>
+        
+    </li>
     )
 }
