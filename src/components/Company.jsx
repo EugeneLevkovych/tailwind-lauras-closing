6e7b50218aa79409e41d1a87bd9d88bcb8efbe75
Eugene <eugenelevkovych@yahoo.com>
2025-05-27 13:04:03 +0300

    add terms 2

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,4 +1,5 @@
 import CompanyLists from "./CompanyLists";
+import Terms from "./Terms";
 
 export default function Company() {
     return (
@@ -8,16 +9,7 @@ export default function Company() {
     </div>
     <div>
         <p className="mb-6 font-sm font-medium tracking-wider text-gray-main">©2010-2022 Laura’s Closet All Rights Reserved</p>
-        <div className="flex gap-3.5 mb-4">
-            <p className="font-sm font-medium tracking-wider text-gray-main underline underline-offset-4 sm:whitespace-nowrap">Privacy Center</p>
-            <p className="font-sm font-medium tracking-wider text-gray-main underline underline-offset-4 sm:whitespace-nowrap">Privacy & Cookie Policy</p>
-            <p className="font-sm font-medium tracking-wider text-gray-main underline underline-offset-4 sm:whitespace-nowrap">Manage Cookies</p>
-            </div>
-        <div className="flex gap-3.5">
-            <p className="font-sm font-medium tracking-wider text-gray-main underline underline-offset-4 sm:whitespace-nowrap">Terms & Conditions</p>
-            <p className="font-sm font-medium tracking-wider text-gray-main underline underline-offset-4 sm:whitespace-nowrap">Copyright Notice</p>
-            <p className="font-sm font-medium tracking-wider text-gray-main underline underline-offset-4 sm:whitespace-nowrap">Imprint</p>
-        </div>
+        <Terms />
     </div>
 </>
 )}
